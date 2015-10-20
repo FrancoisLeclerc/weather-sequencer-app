36c3eb6c342ab4f05bc22d2f2fe3f69322d9447d
Nora Top <top.nora@gmail.com>
2015-10-20 18:31:16 +0000

    fixed sequencerOuput

--- js/modules/sequencer-engine.js
+++ js/modules/sequencer-engine.js
@@ -1,7 +1,7 @@
 var Tone = require("tone");
 
 function sequencerOutput(){
-    var player = new Tone.Player("./path/to/sample.mp3").toMaster();
+    var player = new Tone.Player("./audio/sample.mp3").toMaster();
     Tone.Buffer.onload = function(){
     	player.start();
     }    
