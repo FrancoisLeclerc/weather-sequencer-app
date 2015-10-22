0b98cf94f74392b571b3ff8d64d6129d34660a84
Nora Top <top.nora@gmail.com>
2015-10-22 22:09:31 +0000

    modified rain fx

--- js/modules/fx/rain.js
+++ js/modules/fx/rain.js
@@ -1,13 +1,12 @@
 var Tone = require("tone");
 
-function rain(weather){
+function rainFx(weather){
     
     var precipIntensity = weather.precipIntensity;
     var precipProbability = weather.precipProbability;
-    console.log("precipIntensity");
-    console.log(precipIntensity);
-    console.log("precipProbability");
-    console.log(precipProbability);
+    console.log("precipIntensity: " + precipIntensity);
+    console.log("precipProbability: " + precipProbability);
+
     var delayTime = 0;
     var feedback = 0;
     
@@ -31,8 +30,12 @@ function rain(weather){
 
     if (precipProbability) feedback = precipProbability;
     
-    var pingPong = new Tone.PingPongDelay(delayTime, feedback).toMaster();
+    console.log("--pingPong");
+    console.log("delayTime: "+delayTime);
+    console.log("feedback: "+feedback);
+    
+    var pingPong = new Tone.PingPongDelay(delayTime, feedback);
     return pingPong;
 }
 
-module.exports = rain;
+module.exports = rainFx;
