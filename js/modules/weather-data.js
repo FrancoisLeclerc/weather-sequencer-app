ac2c5418c11bb3bc70eacbf3158d32dbfb5724a6
Karl Soulière-Crépeau <karlysun@gmail.com>
2015-10-23 15:58:46 +0000

    CHanged some CSS, modified seq canvas, etc

--- js/modules/weather-data.js
+++ js/modules/weather-data.js
@@ -6,17 +6,27 @@ function getWeather(pos){
         dataType: "jsonp",
         success: function (forecastInfo) {
             var data = forecastInfo.currently;
-            $(".data1").text(Math.round(data.temperature)+"°F");
-            $(".data1m").text(Math.round((data.temperature-32)*(5/9))+"°C");
-            $(".data2").text(data.precipIntensity+" in/h");
-            $(".data2m").text((data.precipIntensity*25.4)+" mm/h");
-            $(".data3").text(Math.round(data.humidity*100)+"%");
-            $(".data4").text(Math.round(data.windSpeed)+" mph");
-            $(".data4m").text(Math.round(data.windSpeed*1.609344)+" km/h");
-            $(".data5").text(Math.round(data.visibility)+" mi");
-            $(".data5m").text(Math.round(data.visibility*1.609344)+" km");
-            $(".data6").text(Math.round(data.cloudCover*100)+"%");
-            $(".data7").text(Math.round(data.pressure)+" mb");
+            // TEMPERATURE
+            if(typeof data.temperature !== "number"){ $(".data1").text("n/a"); $(".data1m").text("n/a"); }
+            else { $(".data1").text(Math.round(data.temperature)+"°F"); $(".data1m").text(Math.round((data.temperature-32)*(5/9))+"°C"); }
+            //PRECIPITATION
+            if(typeof data.precipIntensity !== "number"){ $(".data2").text("n/a"); $(".data2m").text("n/a"); }
+            else { $(".data2").text(data.precipIntensity+" in/h"); $(".data2m").text((data.precipIntensity*25.4)+" mm/h"); }
+            //HUMIDITY
+            if(typeof data.humidity !== "number"){ $(".data3").text("n/a"); }
+            else { $(".data3").text(Math.round(data.humidity*100)+"%"); }
+            //WIND SPEED
+            if(typeof data.windSpeed !== "number"){ $(".data4").text("n/a"); $(".data4m").text("n/a"); }
+            else { $(".data4").text(Math.round(data.windSpeed)+" mph"); $(".data4m").text(Math.round(data.windSpeed*1.609344)+" km/h"); }
+            //VISIBILITY
+            if(typeof data.visibility !== "number"){ $(".data5").text("n/a"); $(".data5m").text("n/a"); }
+            else { $(".data5").text(Math.round(data.visibility)+" mi"); $(".data5m").text(Math.round(data.visibility*1.609344)+" km"); }
+            //CLOUD COVER
+            if(typeof data.cloudCover !== "number"){ $(".data6").text("n/a"); }
+            else { $(".data6").text(Math.round(data.cloudCover*100)+"%"); }
+            //PRESSURE
+            if(typeof data.pressure !== "number"){ $(".data7").text("n/a"); }
+            else { $(".data7").text(Math.round(data.pressure)+" mb"); }
             getDial(data);
         }
     });
