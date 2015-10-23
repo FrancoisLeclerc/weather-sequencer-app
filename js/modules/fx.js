d7ef2a1ea80368564ff6da25e31a196b12078c2a
Nora Top <top.nora@gmail.com>
2015-10-23 21:58:32 +0000

    added bpm fx

--- js/modules/fx.js
+++ js/modules/fx.js
@@ -7,9 +7,14 @@ var humidityFX = require("./fx/humidity");
 var visibilityFX = require("./fx/visibility");
 var cloudFX = require("./fx/cloud");
 var stormFX = require("./fx/storm");
+var temperatureFX = require("./fx/temperature");
 
 
 function connectFX(instrument, weather){
+    
+    //change the bpm of the Transport
+    temperatureFX(weather);
+    
     return instrument.chain(
     rainFX(weather),
     // humidityFX(weather),
