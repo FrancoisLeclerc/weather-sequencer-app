1fd41edd70ceacce31eef446998d3c893d15848b
Nora Top <top.nora@gmail.com>
2015-10-29 22:53:06 +0000

    fix the wind

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ nx.onload = ui.nexusSetting;
 ui.mainDisplayAnimation();
 
 //create the instrument (async loading audio samples)
-g.setInstru(new Instrument(sampleSet["Default"]));
+g.setInstru(new Instrument(sampleSet["Bulldog"]));
 
 //change the track names in the interface
 ui.displayTrackNames(g.getInstru());
